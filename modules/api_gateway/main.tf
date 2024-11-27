44a848dd3048002fa5b16f5f408b8f2285fc1d56
Jindrich Skupa <jindrich.skupa@gmail.com>
2024-11-27 07:27:54 +0100

    feat(init): initial commit

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -47,5 +47,5 @@ resource "aws_api_gateway_deployment" "this" {
 resource "aws_api_gateway_stage" "this" {
   deployment_id = aws_api_gateway_deployment.this.id
   rest_api_id   = aws_api_gateway_rest_api.this.id
-  stage_name    = "$default"
+  stage_name    = "default"
 }
