cc69c7bb0502549dfaa3a160977c034ad3ac5eb0
Jindrich Skupa <jindrich.skupa@gmail.com>
2024-11-27 07:59:26 +0100

    feat(init): initial commit

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -8,7 +8,7 @@ resource "aws_api_gateway_rest_api" "this" {
 
 resource "aws_api_gateway_resource" "this" {
   parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-  path_part   = "/{proxy+}"
+  path_part   = "{proxy+}"
   rest_api_id = aws_api_gateway_rest_api.this.id
 }
 
@@ -23,7 +23,7 @@ resource "aws_api_gateway_integration" "this" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
   resource_id             = aws_api_gateway_resource.this.id
   http_method             = aws_api_gateway_method.any.http_method
-  integration_http_method = "ANY"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = var.lambda_invoke_arn
 }
