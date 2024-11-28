e4f8eff35b04ed1d3afaf64259a3ff7a08d8849c
Jindrich Skupa <jindrich.skupa@gmail.com>
2024-11-28 08:48:13 +0100

    feat(init): initial commit

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -24,7 +24,6 @@ resource "aws_api_gateway_integration" "this" {
   resource_id             = aws_api_gateway_resource.this.id
   http_method             = aws_api_gateway_method.any.http_method
   integration_http_method = "POST"
-  content_handling        = "CONVERT_TO_TEXT"
   type                    = "AWS_PROXY"
   uri                     = var.lambda_invoke_arn
 }
