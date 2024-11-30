7289fe7c40c4b172ecddf0645847b1e7b6430a00
Jindrich Skupa <jindrich.skupa@gmail.com>
2024-11-30 14:56:39 +0100

    feat(init): initial commit

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -10,14 +10,8 @@ resource "aws_api_gateway_rest_api" "this" {
   minimum_compression_size = -1
 }
 
-resource "aws_api_gateway_resource" "parent" {
-  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
-  path_part   = "/"
-  rest_api_id = aws_api_gateway_rest_api.this.id
-}
-
 resource "aws_api_gateway_resource" "this" {
-  parent_id   = aws_api_gateway_resource.parent.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
   path_part   = "{proxy+}"
   rest_api_id = aws_api_gateway_rest_api.this.id
 }
@@ -32,17 +26,6 @@ resource "aws_api_gateway_method" "any" {
 resource "aws_api_gateway_integration" "this" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
   resource_id             = aws_api_gateway_resource.this.id
-  http_method             = aws_api_gateway_method.any.http_method
-  passthrough_behavior    = "WHEN_NO_MATCH"
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  timeout_milliseconds    = "29000"
-  uri                     = var.lambda_invoke_arn
-}
-
-resource "aws_api_gateway_integration" "this_parent" {
-  rest_api_id             = aws_api_gateway_rest_api.this.id
-  resource_id             = aws_api_gateway_resource.parent.id
   http_method             = aws_api_gateway_method.any.http_method
   passthrough_behavior    = "WHEN_NO_MATCH"
   integration_http_method = "POST"
