d1a867b884b15975e869178bb670f847d552de9b
Jindrich Skupa <jindrich.skupa@gmail.com>
2024-11-30 14:02:26 +0100

    feat(init): initial commit

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -11,7 +11,7 @@ resource "aws_api_gateway_rest_api" "this" {
 }
 
 resource "aws_api_gateway_resource" "parent" {
-  parent_id   = ""
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
   path_part   = ""
   rest_api_id = aws_api_gateway_rest_api.this.id
 }
